d88eef0d48ed441411cc80d9be0165ecce2b533b
Penny Chase <mpchase@mac.com>
2014-06-20 20:38:27 -0400

    Completed comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Functions that enable the memoized computation of the inverse of matrix
-## makeCacheMatrix creates a matrix "object" that can cache the matrix's inverse.
+## Computing the inverse of a matrix using memoization.
+##
+## makeCacheMatrix creates a matrix "object" that can cache its inverse.
 ## cacheSolve computes the inverse of a matrix, first checking to see if there's 
-## a cached result and, if not, computes the matrix with solve() and caches the result.
+## a cached result and, if not, computes the matrix using solve() and caches the result.
 ##
 ## Use as follows:
 ## > m <- makeCacheMatrix(matrix(c(0, 1, 4, 1, 0, -3, 2, 3, 8), 3, 3))
@@ -9,21 +10,28 @@
 ## > m$getinverse()
 
 ## makeCacheMatrix creates a memoized matrix "object" that can cache the results of solving 
-## for the inverse. This function actually creates an environment with a variable, x.inv, that stores
-## the inverse and functions that set and get the matrix and its inverse.
+## for the inverse. The "object" created by this fucntion is really a list of four accessor
+## functions that set and get the matrix and its inverse. They use R's lexical scoping rules to
+## bind variables to the matrix and the inverse of the matrix, and access them.
 ##
-## Arguments: x, a matrix (defaults to an empty matrix)
-## Returns: A list of the functions set, get, setinverse, and getinverse
+## Arguments: x, a matrix (defaults to an empty matrix). Assume x is invertible.
+## Returns: The list of the functions set, get, setinverse, and getinverse
 
 makeCacheMatrix <- function(x = matrix()) {
     x.inv <- NULL
     
-    set <- function(y) {      # define set, get, setinverse, getinverse
+    # set binds x, the variable that stores the matrix, to its argument, y and resets the
+    # inverse to NULL. It uses "<<-" to search the parent environments and change the
+    # definitions of the variables.
+    set <- function(y) {      
             x <<- y
             x.inv <<- NULL
     }
+    # get returns the matrix stored in x
     get <- function() x
+    # setinverse binds x.inv, the inverse of x, to its argument
     setinverse <- function(inverse) x.inv <<- inverse
+    # getinverse returns the inverse of x
     getinverse <- function() x.inv
     
     list(set = set, get = get,     # return a list of the functions
@@ -32,17 +40,23 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of a matrix. The frst time it's called it will cache 
+## the inverse, and subsequently will return the cached result.
+##
+## Arguments: x, a matrix "object" created by makeCacheMatrix
+##            ..., arguments to pass to solve()
+## Returns: the inverse of the matrix, x
 
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
     m <- x$getinverse()
+    # if the inverse has been cached, return it
     if(!is.null(m)) {
             message("getting cached data")
             return(m)
     }
+    # otherwise, get the actual matrix and use solve() to compute the inverse
     data <- x$get()
     m <- solve(data, ...)
-    x$setinverse(m)
+    x$setinverse(m)    # cache the result
     m
 }
